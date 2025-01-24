aee1f886f75e16716ad811cf3515a19d52c64dcc
ManiacD16 <msd161918@gmail.com>
2025-01-24 13:32:23 +0530

    first commit

--- services/qrCodeService.js
+++ services/qrCodeService.js
@@ -1,3 +1,18 @@
+// const qrCode = require("qrcode");
+// const path = require("path");
+
+// const generateQRCode = async (address) => {
+//   // Save QR code in the 'public/qr-codes' directory
+//   const qrPath = path.join(__dirname, "../public/qr-codes", `${address}.png`);
+//   const qrLink = `https://itc.trademarketcap.ai/${address}`; // URL encoded in the QR code
+//   await qrCode.toFile(qrPath, qrLink); // Save QR code image at the desired path
+
+//   // Return the relative URL to access the QR code from the public folder
+//   return `/qr-codes/${address}.png`; // URL to access the file
+// };
+
+// module.exports = { generateQRCode };
+
 // qrCodeService.js
 const qrCode = require("qrcode");
 const { v2: cloudinary } = require("cloudinary");
